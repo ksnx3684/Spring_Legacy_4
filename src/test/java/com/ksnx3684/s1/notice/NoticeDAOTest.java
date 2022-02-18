a29529ad29b02c73b1380515ec4f3b564d9ba312
ksnx3684 <ksnx3684@naver.com>
2022-02-18 09:28:53 +0900

    Create Notice

---
+++ src/test/java/com/ksnx3684/s1/notice/NoticeDAOTest.java
@@ -0,0 +1,60 @@
+package com.ksnx3684.s1.notice;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ksnx3684.s1.MyJunitTest;
+
+public class NoticeDAOTest extends MyJunitTest {
+
+	@Autowired
+	private NoticeDAO noticeDAO;
+	
+	@Test
+	public void check() {
+		assertNotNull(noticeDAO);
+	}
+	
+//	@Test
+	public void listTest() throws Exception {
+		List<NoticeDTO> ar = noticeDAO.list();
+		
+		assertNotEquals(0, ar.size());
+	}
+	
+//	@Test
+	public void detailTest() throws Exception {
+		NoticeDTO noticeDTO = new NoticeDTO();
+		noticeDTO.setNum(1L);
+		noticeDTO = noticeDAO.detail(noticeDTO);
+		
+		assertNotNull(noticeDTO);
+	}
+	
+	@Test
+	public void addTest() throws Exception {
+		NoticeDTO noticeDTO = new NoticeDTO();
+		noticeDTO.setNum(1L);
+		noticeDTO.setTitle("t1");
+		noticeDTO.setContents("C1");
+		noticeDTO.setWriter("w1");
+		noticeDTO.setRegDate(null);
+		noticeDTO.setHit(1);
+		int result = noticeDAO.add(noticeDTO);
+		
+		assertEquals(1, result);
+	}
+
+//	@Test
+	public void deleteTest() throws Exception {
+		NoticeDTO noticeDTO = new NoticeDTO();
+		noticeDTO.setNum(1L);
+		int result = noticeDAO.delete(noticeDTO);
+		
+		assertEquals(1, result);
+	}
+}
